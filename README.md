7e93d36aede5358a54cadf8cd247a24fde80935f
Voldemalort <voldemalort@gmail.com>
2020-07-09 16:35:16 -0500

    major revision - no longer manipulates lights, just the blend mode of the player's dimvision radius to turn overlapping dim light to bright light

--- README.md
+++ README.md
@@ -3,25 +3,17 @@
 This module attempts to replicate `dnd5e` darkvision rules more accurately, leveraging the existing `dim vision` setting on tokens' vision tabs.
 
 The default Foundry VTT behavior when `dim vision` is set does the following:
+
 * Renders `darkness` within the `dim vision` radius as `dim light`
 * Renders `dim light` within the `dim vision` radius as `dim light`
 * Renders `bright light` within the `dim vision` radius as `bright light`
 
 Darkvision rules as written for `dnd5e` actually increase each level of light below bright light by one step within the `dim vision` radius. The desired behavior is:
+
 * Renders `darkness` within the `dim vision` radius as `dim light`
 * **Renders `dim light` within the `dim vision` radius as `bright light`**
 * Renders `bright light` within the `dim vision` radius as `bright light`
 
-This module accomplishes the desired `dnd5e` behavior by iterating through the light-sources of a scene when a token is controlled or updated, doing some math to determine if a light is within the `dim vision` radius, and if it is, temporarily setting the light-source's bright radius to the maximum of the dim and bright radius for the light.
-
-## Configuration
-
-Darker Vision can be configured to brighten lights when:
-* any light emitted by the light-source enters the `dim vision` radius of a token
-* any `bright light` emitted by the light-source enters the `dim vision` radius of a token
-* the light-source origin enters the `dim vision` radius of a token
-
 ## Known issues
 
 * Selecting multiple tokens simultaneously will render lighting inconsistently.
-* This likely won't work with Dancing Lights or similar Foundry VTT modules that update light-sources often.
--- darker-vision-for-5e.js
+++ darker-vision-for-5e.js
@@ -1,89 +1,37 @@
 const DarkerVisionFor5e = {
-  register: () => {
-    game.settings.register('darker-vision-for-5e', 'brighten-boundary', {
-      name: 'What part of a light source must be within the dim vision radius to be brightened?',
-      scope: 'world',
-      config: true,
-      default: 'any',
-      type: String,
-      choices: {
-        any: 'Any light (bright or dim)',
-        bright: 'Any bright light (or the light source origin if it emits only dim light)',
-        origin: 'The light source origin',
-      },
-    });
-  },
-
-  initialize: () => {
-    DarkerVisionFor5e.modifyLights();
-  },
+  sightLayerUpdate: SightLayer.prototype.update,
 
   patch: {
     removeLitTokenRemnants: (tokenId) => {
       canvas.sight.sources.lights.delete(`Token.${tokenId}`);
       canvas.sight.update();
       canvas.lighting.update();
+      DarkerVisionFor5e.betterDimVision();
     },
   },
 
-  modifyLights: (ambientLight) => {
+  betterDimVision: () => {
     const controlledToken = canvas.tokens.controlled[0];
-    const brightenBoundary = game.settings.get('darker-vision-for-5e', 'brighten-boundary');
+    const dimLights = canvas.sight.light.dim.children;
 
-    if (ambientLight) {
-      const updatedLight = canvas.sight.sources.lights.get(`Light.${ambientLight._id}`);
-      updatedLight.originalBright = ambientLight.bright * 0.2 * canvas.scene.data.grid;
+    if (controlledToken
+      && controlledToken.data.dimSight
+      && dimLights.length
+      && !dimLights[dimLights.length - 1].isToken
+    ) {
+      dimLights[0].isToken = true;
+      dimLights[0].light.blendMode = PIXI.BLEND_MODES.SUBTRACT;
+      dimLights.push(dimLights.shift());
     }
-
-    canvas.sight.sources.lights.forEach((light) => {
-      let shouldBrighten = false;
-
-      if (controlledToken && controlledToken.data.dimSight) {
-        const vision = canvas.sight.sources.vision.get(`Token.${controlledToken.id}`);
-        const xOffset = Math.abs(light.x - vision.x);
-        const yOffset = Math.abs(light.y - vision.y);
-        const distance = Math.sqrt((xOffset * xOffset) + (yOffset * yOffset));
-        const dimVision = vision.channels.dim;
-        const dimLight = light.channels.dim;
-        const brightLight = light.channels.bright;
-
-        switch (brightenBoundary) {
-          case 'bright':
-            shouldBrighten = distance <= (dimVision + brightLight);
-            break;
-          case 'origin':
-            shouldBrighten = distance <= dimVision;
-            break;
-          default:
-            shouldBrighten = distance <= (dimVision + Math.max(brightLight, dimLight));
-        }
-      }
-
-      light.originalBright = light.originalBright || light.channels.bright;
-
-      if (shouldBrighten) {
-        light.channels.bright = Math.max(light.channels.bright, light.channels.dim);
-      } else {
-        light.channels.bright = light.originalBright;
-      }
-    });
-
-    canvas.sight.update();
   },
 };
 
-Hooks.once('init', () => DarkerVisionFor5e.register());
+SightLayer.prototype.update = function update() {
+  DarkerVisionFor5e.sightLayerUpdate.bind(this)();
+  DarkerVisionFor5e.betterDimVision();
+};
 
+// remove after fixed in FVTT v0.7.0
 Hooks.on('ready', () => {
-  DarkerVisionFor5e.initialize();
-
-  Hooks.on('controlToken', () => DarkerVisionFor5e.modifyLights());
-  Hooks.on('updateToken', () => DarkerVisionFor5e.modifyLights());
-  Hooks.on('createToken', () => DarkerVisionFor5e.modifyLights());
-
-  Hooks.on('updateAmbientLight', (scene, light) => DarkerVisionFor5e.modifyLights(light));
-  Hooks.on('creatAmbientLight', (scene, light) => DarkerVisionFor5e.modifyLights(light));
-
-  // remove after fixed in FVTT v0.7.0
   Hooks.on('deleteToken', (scene, token) => DarkerVisionFor5e.patch.removeLitTokenRemnants(token._id));
 });
